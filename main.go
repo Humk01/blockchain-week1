5d44fcbfe8272597a7ae484cd74310e4b770778b
Humk <bitrusseth06@gmail.com>
2026-05-23 22:04:35 +0100

    Add bitcoin validation to detect tampering

--- main.go
+++ main.go
@@ -60,6 +60,27 @@ func (bc *BlockChain) addBlock(data string) {
 	bc.Blocks = append(bc.Blocks, newBlock)
 }
 
+// this function checks if the blockchain was tampered with
+
+func (bc *BlockChain) isValid() bool {
+	// checks each block (starting from block 1)
+	for i := 1; i < len(bc.Blocks); i++ {
+		currentBlock := bc.Blocks[i]
+		previousBlock := bc.Blocks[i-1]
+
+		if currentBlock.Hash != currentBlock.CalculateHash() {
+			fmt.Printf("Block #%d has invalid hash!\n", currentBlock.INDEX)
+			return false
+		}
+
+		if currentBlock.PreviousHash != previousBlock.Hash {
+			fmt.Printf("Block #%d is not properly linked to previous block!\n", currentBlock.INDEX)
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	// create blockchain starting with the genesis block
 	blockchain := BlockChain{
@@ -71,10 +92,15 @@ func main() {
 	blockchain.addBlock("Bob sent 5 BTC to Charlie")
 	blockchain.addBlock("Charlie sent 2 BTC to Alice")
 
+	// Validate
+	fmt.Println("Is blockchain valid?", blockchain.isValid())
+
 	for _, block := range blockchain.Blocks {
 		fmt.Printf("Block #%d\n", block.INDEX)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %s\n", block.Hash)
 		fmt.Printf("Previous Hash: %s\n\n", block.PreviousHash)
 	}
+
+	
 }
